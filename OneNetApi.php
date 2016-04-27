f5e5a12f378e506b481edeea0978db70b91db3bd
LiMao <limao@iot.chinamobile.com>
2016-04-27 09:33:36 +0800

    add timeout

--- OneNetApi.php
+++ OneNetApi.php
@@ -638,7 +638,11 @@ class OneNetApi
         }
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 2);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 1);
+		if($method != 'GET'){
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+		}
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
         // header set
